38702edd5d5ce3100dd5f2b87e0b8b30de9902c7
Fabio de Albuquerque Dela Antonio <fabio914@gmail.com>
2026-01-06 23:35:03 +0000

    WIP New syntax

--- Input/Input.swift
+++ Input/Input.swift
@@ -1,6 +1,13 @@
 import Foundation
 import SwiftUI
 
+// wiring: container(MyContainer) {
+//   bind(MyClass, SomeProtocol)
+//   singletonBind(MySingleton, SomeOtherProtocol)
+//   singleton(AnotherSingleton)
+//   instance(SomeInstance)
+//   instance(SomeInstanceWithoutParameters)
+// }
 @Container(MyContainer)
 @Bind(MyClass, SomeProtocol)
 @SingletonBind(MySingleton, SomeOtherProtocol)
@@ -10,6 +17,7 @@ import SwiftUI
 protocol MyContainerProtocol {
 }
 
+// wiring: inject
 @Inject
 final class MyClass: SomeProtocol {
 
@@ -20,12 +28,17 @@ final class MyClass: SomeProtocol {
     let parameter: Int
 
     init(
+        // wiring: dependency
         @Dependency instance: SomeInstanceWithoutParameters,
+        // wiring: dependency
         @Dependency someDependency: SomeDependency,
+        // wiring: dependency
         @Dependency anotherDependency: AnotherDependency,
+        // wiring: dependency
         @Dependency singleton: SomeOtherProtocol,
         parameter value: Int,
         otherParameter: Array<Int>,
+        // wiring: dependency
         @Dependency container: MyContainerProtocol
     ) {
         self.instance = instance
@@ -44,35 +57,41 @@ final class MyClass: SomeProtocol {
 // Singletons can only depend on singletons, external dependencies,
 // or dependencies without parameters, or have no dependencies.
 
+// wiring: inject
 @Inject
 class MySingleton: SomeOtherProtocol {
 
     let someDependency: SomeDependency
 
     init(
+        // wiring: dependency
         @Dependency someDependency: SomeDependency
     ) {
         self.someDependency = someDependency
     }
 }
 
+// wiring: inject
 @Inject
 class AnotherSingleton {
     let firstSingleton: SomeOtherProtocol
 
     init(
+        // wiring: dependency
         @Dependency firstSingleton: SomeOtherProtocol
     ) {
         self.firstSingleton = firstSingleton
     }
 }
 
+// wiring: inject
 @Inject
 class SomeInstance {
     let firstSingleton: SomeOtherProtocol
     let parameter: Int
 
     init(
+        // wiring: dependency
         @Dependency firstSingleton: SomeOtherProtocol,
         parameter: Int
     ) {
@@ -81,13 +100,16 @@ class SomeInstance {
     }
 }
 
+// wiring: inject
 @Inject
 class SomeInstanceWithoutParameters {
     let firstSingleton: SomeOtherProtocol
     let anotherSingleton: AnotherSingleton
 
     init(
+        // wiring: dependency
         @Dependency firstSingleton: SomeOtherProtocol,
+        // wiring: dependency
         @Dependency anotherSingleton: AnotherSingleton
     ) {
         self.firstSingleton = firstSingleton
